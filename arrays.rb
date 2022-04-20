84a8be91bc4119316b76f9cea93518715e22f0cf
alepbloyd <alexanderleeboyd@gmail.com>
2022-04-20 11:09:50 -0400

    Add arrays of data types

---
+++ arrays.rb
@@ -0,0 +1,15 @@
+#string array
+
+array_of_strings = ["this is a string","this is another string","this is a third string","and a fourth string"]
+
+#integer array
+
+array_of_integers = [1,2,3,4,5]
+
+#float array
+
+array_of_floats = [1.1,2.2,3.3,4.4,5.5]
+
+#boolean array
+
+array_of_booleans = [true,false,true,false]
