61a657553fdacf154d3874019a760cfe94bcfcc4
alepbloyd <alexanderleeboyd@gmail.com>
2022-04-20 11:18:27 -0400

    Add array method examples and explanatory comments

--- arrays.rb
+++ arrays.rb
@@ -1,15 +1,15 @@
-#string array
-
 array_of_strings = ["this is a string","this is another string","this is a third string","and a fourth string"]
-
-#integer array
+array_of_strings.pop
+#this will return the last element of array_of_strings, and remove the last element of array_of_strings
 
 array_of_integers = [1,2,3,4,5]
-
-#float array
+array_of_integers.push(6,7,8)
+#this will add the elements 6,7, and 8 to the end of array_of_integers and return the modified array
 
 array_of_floats = [1.1,2.2,3.3,4.4,5.5]
-
-#boolean array
+array_of_floats.shift
+#this will return the first element of array_of_floats and return it
 
 array_of_booleans = [true,false,true,false]
+array_of_booleans.unshift(false)
+#this will add the boolean 'false' as the first element of array_of_booleans, and return the modified array
