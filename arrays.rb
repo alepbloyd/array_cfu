b1d81f47ef94fd570954547c7f95b191b6dae711
alepbloyd <alexanderleeboyd@gmail.com>
2022-04-20 11:25:15 -0400

    Add index understanding examples

--- arrays.rb
+++ arrays.rb
@@ -8,8 +8,19 @@ array_of_integers.push(6,7,8)
 
 array_of_floats = [1.1,2.2,3.3,4.4,5.5]
 array_of_floats.shift
-#this will return the first element of array_of_floats and return it
+#this will return the first element of array_of_floats and remove it
 
 array_of_booleans = [true,false,true,false]
 array_of_booleans.unshift(false)
 #this will add the boolean 'false' as the first element of array_of_booleans, and return the modified array
+
+#index understanding examples
+
+array_of_integers[array_of_integers.length-1]
+#above example will always return the final element in array_of_integers, because it gets the number of elements, and subtracts 1 to get the index of final element
+
+array_of_floats[0]
+#above example will return the first element of array_of_floats (2.2 because called shift on initial array)
+
+array_of_booleans[5]
+#above example will return nil because there is no array_of_booleans[5]
